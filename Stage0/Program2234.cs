e1ac311d03616633ab07e9577c14766bbf26b558
chaniKirshtein2003 <ck4112611@gmail.com>
2022-10-30 19:03:40 +0200

    Initial code + welcome functions

---
+++ Stage0/Program2234.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2234();
+            welcome2221();
+        }
+        static partial void welcome2221();
+        private static void welcome2234()
+        {
+            Console.Write("Enter your name: ");
+            string myName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", myName);
+        }
+    }
+}
