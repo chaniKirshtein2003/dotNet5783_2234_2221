71b4dfb1a5797038f64497ee1d74613a68cd49f3
mini <minigoldshmid@gmail.com>
2023-01-12 20:34:01 +0200

    convert

---
+++ PL/Converters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PL
+{
+    public class NotEmptyTextToEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text=(string)value;
+            return (text == null || text == "") ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
